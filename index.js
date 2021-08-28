3eb41312c1c910e6940338d96cae242865f2a377
Seth Ryan Watkins <seth.watkins.000@gmail.com>
2021-08-27 20:35:08 -0500

    the base

---
+++ index.js
@@ -0,0 +1,70 @@
+import { useEffect, useMemo, useState } from 'react'
+
+const useApi = (promise, effects) => {
+  const [executed, setExecuted] = useState([])
+
+  const { onSuccess, onError, parseResult, parseError } = effects
+
+  const status = useMemo(() => {
+    const finished = executed.filter(exec => exec.end)
+    const mostRecent = [...finished].sort(exec => exec.start).reverse()[0] || {}
+    return {
+      ...mostRecent,
+      loading:
+        executed.length > finished.length &&
+        (executed.length === 1 ||
+          executed.some(exec => exec.start > mostRecent.start)),
+    }
+  }, [executed, onSuccess, onError])
+
+  useEffect(() => {
+    if (executed.length > 1 && executed.every(exec => exec.end)) {
+      const mostRecent = [...executed].sort(exec => exec.start).reverse()[0]
+      setExecuted([mostRecent])
+    }
+  }, [executed])
+
+  const request = async (params = []) => {
+    const start = Date.now()
+    setExecuted(prev => [...prev, { start }])
+    try {
+      const res = await promise(...params)
+      const result = (await parseResult?.(res)) ?? res
+      await onSuccess?.(result)
+      setExecuted(prev =>
+        prev.reduce((next, exec) => {
+          exec.start === start
+            ? next.push({
+                ...exec,
+                end: Date.now(),
+                result,
+              })
+            : next.push(exec)
+          return next
+        }, [])
+      )
+    } catch (e) {
+      const error = (await parseError?.(e)) ?? e
+      await onError?.(error)
+      setExecuted(prev =>
+        prev.reduce((next, exec) => {
+          exec.start === start
+            ? next.push({
+                ...exec,
+                end: Date.now(),
+                error,
+              })
+            : next.push(exec)
+          return next
+        }, [])
+      )
+    }
+  }
+
+  return {
+    request,
+    ...status,
+  }
+}
+
+export default useApi
